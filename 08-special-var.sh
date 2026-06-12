e93ef58de3fa69a7e47617752622e2264f4628b1
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-12 11:50:48 +0530

    shell-practice

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -5,3 +5,15 @@ echo "pass all arguments into script: $@"
 echo "pass all arguments into script: $*"
 
 echo "the script name is: $0"
+
+echo "current working directory of the script : $PWD"
+
+echo "who is running the script : $USER"
+
+echo "home directory of the script: $HOME"
+
+echo "PID of the Script : $!"
+
+sleep 50 & # sleep 50 secs in baground
+
+echo "PID of the last command: $$"

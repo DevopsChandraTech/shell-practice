166f484098b60333ea81e46a1cbfcd4d6ddeb318
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-12 16:24:51 +0530

    shell-practice

--- 12.installations.sh
+++ 12.installations.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+if [ USERID -ne 0 ]; then
+    echo "Error:: command run with root user privilizes"
+fi
+
+dnf install mysql -y
+
 if [ $? -ne 0 ]; then
-    echo "mysql not installed..! Proceed"
-    dnf install mysql -y
-else 
-    echo "mysql already installed..!"
+    echo "Error:: Please check once the command."
+else
+    echo "mysql installing successfully."
 fi

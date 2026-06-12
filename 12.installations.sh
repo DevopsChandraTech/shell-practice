cc471afd08d55804ddfa8e6e85dbda08bf868fd7
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-12 16:25:49 +0530

    shell-practice

--- 12.installations.sh
+++ 12.installations.sh
@@ -2,14 +2,16 @@
 
 USERID=$(id -u)
 
-if [ USERID -ne 0 ]; then
+if [ $USERID -ne 0 ]; then
     echo "Error:: command run with root user privilizes"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "Error:: Please check once the command."
+    exit 1
 else
     echo "mysql installing successfully."
 fi

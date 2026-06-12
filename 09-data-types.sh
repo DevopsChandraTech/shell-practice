f2bf85a7e8462ccea9bbc105c4c070befc78283e
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-12 12:53:06 +0530

    shell-practice

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -7,3 +7,7 @@ NAME=Chandra
 
 SUM=$(($NUMBER1 + $NUMBER2 + $NAME))
 echo "sum of the numbers is: ${SUM}"
+
+FAMILY=("Amma","Chandra","Anu","Gnapika","Hansith")
+
+echo "all members of the family is : $@"

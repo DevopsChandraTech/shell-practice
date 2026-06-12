d57082ecdb981fc905e8f56c3502ab58d47aac49
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-12 10:44:59 +0530

    shell-practice

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+# PERSON1=$1
+# PERSON2=$2
 
-echo "$PERSON1:: HELLO $PERSON2"
-echo "$PERSON2:: HELLO $PERSON1"
+# echo "$PERSON1:: HELLO $PERSON2"
+# echo "$PERSON2:: HELLO $PERSON1"
+
+echo "please enter you pin number"
+
+read PINNUMBER
+
+echo "your pin is $PINNUMBER"
 

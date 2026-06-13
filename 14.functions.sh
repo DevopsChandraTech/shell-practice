7930c0c280a2b1e8f80234db2efa1d56ca89b0c6
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-13 20:12:43 +0530

    shell-practice

--- 14.functions.sh
+++ 14.functions.sh
@@ -2,32 +2,23 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then #here userid = 0 that is root user other than 0 not an root user.
-    echo "Error::Plese run the command with root user privilizes."
-    exit 1 # 0 is the success other than 0 is error output
+if [ $USERID -ne 0 ]; then
+    echo "Error:: Run command with root user privilizes."
 fi
 
 VALIDATE(){
-   if [ $1 -ne 0 ]; then # functions take inputs from args like shell scripting args.
-        echo "Error:: Command Not found Plese check once the command."
-        exit 1
+    if [ $1 -ne 0 ]; then
+        echo "Error:: MySql Not installed."
     else    
-        echo "Installed Successfully." # we tell echo statement to enable after \
-        # without e generally echo statement print the inside the "" quotes.
-    fi 
+        echo "$2 Already installed...! Skipping."
+    fi
 }
-
-# mysql installation:
 
 dnf install mysql -y
 VALIDATE $? MySql
 
-# Nginx Installation:
-
 dnf install nginx -y
 VALIDATE $? Nginx
-
-# python3 installation
 
 dnf install python3 -y
 VALIDATE $? Python3

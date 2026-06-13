951532b02c63557ebc2863f6607437940404a4bf
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-13 23:01:05 +0530

    shell-practice

--- 17-logs-redir.sh
+++ 17-logs-redir.sh
@@ -13,6 +13,13 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | awk -F "." '{print $1}') # (or echo $0 | cut -d '.' -f1)
+FILE_NAME="/$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
 VALIDATE(){
     if [ $1 -ne 0 ]; then # $1 is first arg for $? and $2 is 2nd arg for MySql
         echo -e "Failure:: $R $2 $N Command Not Found."
@@ -23,38 +30,39 @@ VALIDATE(){
 }
 
 #mysql installation
-dnf list installed mysql # check the list of installed packages available
+dnf list installed mysql &>> $FILE_NAME # check the list of installed packages available
 if [ $? -ne 0 ]; then
-    dnf install mysql -y
+    dnf install mysql -y &>> $FILE_NAME
     VALIDATE $? MySql
 else 
     echo -e "MySql already installed $Y Skipping...$N"
 fi
 
 #nginx installation
-dnf list installed nginx
+dnf list installed nginx &>> $FILE_NAME
 if [ $? -ne 0 ]; then
-    dnf install nginx -y
+    dnf install nginx -y &>> $FILE_NAME
     VALIDATE $? Nginx
 else 
     echo -e "Nginx already installed $Y Skipping...$N"
 fi
 
 #Python3 installation
-dnf list installed python3 
+dnf list installed python3 &>> $FILE_NAME
 if [ $? -ne 0 ]; then
-    dnf install python3 -y
+    dnf install python3 -y &>> $FILE_NAME
     VALIDATE $? Python3
 else 
     echo -e "Python3 already installed $Y Skipping...$N"
 fi
 
 #Unzip installation
-dnf list installed unzip 
+dnf list installed unzip &>> $FILE_NAME
 if [ $? -ne 0 ]; then
-    dnf install unzip -y
+    dnf install unzip -y &>> $FILE_NAME
     VALIDATE $? Unzip
 else 
     echo -e "Unzip already installed $Y Skipping...$N"
 fi
+
 

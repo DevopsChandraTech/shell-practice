e9d93f86f1d167f5eeaafb7deeb940229e875c5a
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-13 21:12:48 +0530

    shell-practice

--- 16-colour-functions.sh
+++ 16-colour-functions.sh
@@ -8,13 +8,13 @@ N="\e[0m"
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-    echo "$RError$N :: run command with root user privilizes."
+    echo -e "$RError$N :: run command with root user privilizes."
     exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "Failure:: $R $2 $N Command Not Found."
+        echo -e "Failure:: $R $2 $N Command Not Found."
     else
         echo "Success:: Proceed with installation."
     fi
@@ -25,5 +25,5 @@ if [ $? -ne 0 ]; then
     dnf install mysql -y
     VALIDATE $? MySql
 else 
-    echo "MySql already installed $Y Skipping...$N"
+    echo -e "MySql already installed $Y Skipping...$N"
 

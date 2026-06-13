25eb86d91d84e43a52a6a603b6146c6939e896e8
DevOpsChandraTech <devopschandra42@gmail.com>
2026-06-13 18:47:04 +0530

    shell-practice

--- 14.functions.sh
+++ 14.functions.sh
@@ -4,11 +4,13 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then #here userid = 0 that is root user other than 0 not an root user.
     echo "Error::Plese run the command with root user privilizes."
+    exit 1 # 0 is the success other than 0 is error output
 fi
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then # functions take inputs from args like shell scripting args.
         echo "Error:: Command Not found Plese check once the command."
+        exit 1
     else    
         echo "$2 Installed Successfully."
     fi 
